4a8036d0e19a13d4b352c4a2098d4942c51c3820
pashavik <pash.petrow2011@yandex.ru>
2019-03-13 22:07:53 +0300

    url

---
+++ lesson5/src/readURL/Main.java
@@ -0,0 +1,37 @@
+package readURL;
+
+import com.sun.jndi.toolkit.url.Uri;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            Scanner sc=new Scanner(System.in);
+
+            System.out.println("введите url");
+            String url=sc.nextLine();
+            read(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void read(String url) throws IOException {
+        URL u=new URL(url);
+        BufferedReader reader=new BufferedReader(new InputStreamReader(u.openStream()));
+        String input;
+        while ((input=reader.readLine())!=null)
+            System.out.println(input);
+            reader.close();
+
+
+    }
+}
